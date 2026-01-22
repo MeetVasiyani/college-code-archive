93e7c60989ef4d75a573316e011555e178d11bbc
MeetVasiyani <meetvasiyani.work@gmail.com>
2026-01-22 22:17:10 +0530

    Practice

---
+++ Java practice/Leetcode/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int result = 0;
+
+        while (i < j) {
+            result = Math.max(result, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return result;
+
+    }
+}
