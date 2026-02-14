d41edfcb07bc840a97cf16d967a91e7379973600
MeetVasiyani <meetvasiyani.work@gmail.com>
2026-02-14 17:20:19 +0530

    Practice

---
+++ Java practice/Basics/Question_04.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+
+        if((n & 1) == 1){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+    }
+}
