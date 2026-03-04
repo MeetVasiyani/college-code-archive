86dcdcc1c18208fdf514e4d33e4634f5dc110110
MeetVasiyani <meetvasiyani.work@gmail.com>
2026-03-04 17:11:58 +0530

    Practice

---
+++ Java practice/Basics/Question_25.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+      
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= 2*(n-i-1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
