ede4b62fe6c195df453c4a64456b3d52a5788f03
MeetVasiyani <meetvasiyani.work@gmail.com>
2026-03-20 18:57:35 +0530

    Practice

---
+++ Java practice/Basics/Question_35.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Main{
+    public static void reverse(int [] arr,int left,int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    } 
+    public static void rotate(int [] arr, int k){
+        int n = arr.length;
+        k = k % n;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);        
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter number for index "+(i+1)+" : ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter rotation steps: ");
+        int k = sc.nextInt();
+        
+        rotate(arr,k);
+
+        System.out.println("Array : ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
